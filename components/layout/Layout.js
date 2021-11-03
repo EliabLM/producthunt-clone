f320db1a69fd848afbafd1d1f04ad7a8354c13e7
eliablm <eliablopez@hotmail.com>
2021-11-03 11:26:39 -0500

    Agrega routing en next

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,9 +1,16 @@
 import React, { Fragment } from 'react';
+import Link from 'next/link';
 
 const Layout = (props) => {
   return (
     <Fragment>
       <h1>Header</h1>
+
+      <nav>
+        <Link href="/">Inicio</Link>
+        <Link href="/nosotros">Nosotros</Link>
+      </nav>
+
       <main>{props.children}</main>
     </Fragment>
   );
