1741213d13b3c0b211f251f408c0e01e6d92a73a
eliablm <eliablopez@hotmail.com>
2021-11-05 17:18:41 -0500

    Agrega formulario para crear cuenta

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -5,6 +5,33 @@ const CrearCuenta = () => {
   return (
     <Layout>
       <h1>Crear cuenta</h1>
+
+      <form>
+        <div>
+          <label htmlFor="nombre">Nombre</label>
+
+          <input type="text" id="nombre" placeholder="Nombre" name="nombre" />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+
+          <input type="email" id="email" placeholder="Email" name="email" />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            name="password"
+          />
+        </div>
+
+        <input type="submit" value="Crear Cuenta" />
+      </form>
     </Layout>
   );
 };
