5d6de7452d2aaf3fc4eb131e85c76dde75072aba
eliablm <eliablopez@hotmail.com>
2021-11-09 09:54:04 -0500

    Agrega configuración inicial de firebase

---
+++ firebase/config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDkZaTdMxUxSu6ySTGPNWqJJafJjQeZp-Y',
+  authDomain: 'product-hunt-clone-98db1.firebaseapp.com',
+  projectId: 'product-hunt-clone-98db1',
+  storageBucket: 'product-hunt-clone-98db1.appspot.com',
+  messagingSenderId: '112087968131',
+  appId: '1:112087968131:web:0c7699f177ce8adccfaeb6',
+};
+
+export default firebaseConfig;
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
